2090f910090ecb8b2f6b61f515ab7cca7df8e7e2
miguelguti <miguelgutierrezruano@gmail.com>
2023-03-28 12:50:43 +0200

    added libraries
    created first window

--- code/sources/main.cpp
+++ code/sources/main.cpp
@@ -1,5 +1,38 @@
 
+// Distributed under MIT License
+// @miguelgutierrezruano
+// 2023
+
+#include <SFML/Window.hpp>
+
+using namespace sf;
+
 int main()
 {
+	// Create the window
+	constexpr auto window_width = 800u;
+	constexpr auto window_height = 600u;
+
+	Window window(VideoMode(window_width, window_height), "MGSceneLoader", Style::Titlebar | Style::Close);
+
+	window.setVerticalSyncEnabled(true);
+
+    // Run the main loop:
+
+    bool exit = false;
+
+    do
+    {
+        Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed) exit = true;
+        }
+
+        window.display();
+
+    } while (not exit);
+
 	return 0;
 }
